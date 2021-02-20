666bb26cc1f96b0c59177674150d6f65cf97b56c
Spiros Stampoulis <sstaboulis@gmail.com>
2021-02-20 15:26:11 +0100

    tracker location

---
+++ src/lib/coronavirus.js
@@ -0,0 +1,125 @@
+import { commafy, friendlyDate } from 'lib/util';
+
+/**
+ * trackerLocationToFeature
+ * @param {object} location - Coronavirus Tracker location object
+ */
+
+export function trackerLocationToFeature( location = {}) {
+  const { countryInfo = {} } = location;
+  const { lat, long: lng, iso2, flag } = countryInfo;
+
+  const countryCode = iso2;
+
+  let countryBounds;
+
+  return {
+    type: 'Feature',
+    properties: {
+      ...location,
+      countryCode,
+      countryBounds,
+      flag,
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [lng, lat],
+    },
+  };
+}
+
+/**
+ * trackerLocationsToGeoJson
+ * @param {array} locations - Coronavirus Tracker location objects array
+ */
+
+export function trackerLocationsToGeoJson( locations = []) {
+  if ( locations.length === 0 ) return;
+
+  return {
+    type: 'FeatureCollection',
+    features: locations.map(( location = {}) => trackerLocationToFeature( location )),
+  };
+}
+
+/**
+ * trackerFeatureToHtmlMarker
+ */
+
+export function trackerFeatureToHtmlMarker({ properties = {} } = {}) {
+  const { country, updated, flag, cases, deaths, recovered } = properties;
+
+  let header = country;
+
+  if ( flag ) {
+    header = `<img src="${flag}" name="flag"> ${header}`;
+  }
+
+  let stats = [
+    {
+      label: 'Confirmed',
+      value: cases,
+      type: 'number',
+    },
+    {
+      label: 'Deaths',
+      value: deaths,
+      type: 'number',
+    },
+    {
+      label: 'Recovered',
+      value: recovered,
+      type: 'number',
+    },
+    {
+      label: 'Last Update',
+      value: updated,
+      type: 'date',
+    },
+  ];
+
+  stats = stats.map(( stat ) => {
+    let value = stat?.value;
+
+    if ( !value ) return stat;
+
+    let newValue = value;
+
+    if ( stat?.type === 'number' ) {
+      newValue = commafy( value );
+      if ( value > 999999 ) {
+        newValue = `${newValue.slice( 0, -8 )}m+`;
+      } else if ( value > 999 ) {
+        newValue = `${newValue.slice( 0, -4 )}k+`;
+      }
+    } else if ( stat?.type === 'date' ) {
+      newValue = friendlyDate( newValue );
+    }
+
+    return {
+      ...stat,
+      value: newValue,
+    };
+  });
+
+  let statsString = '';
+
+  stats.forEach(({ label, value }) => {
+    statsString = `
+      ${statsString}
+      <li><strong>${label}:</strong> ${value}</li>
+    `;
+  });
+
+  const casesString = stats.find(({ label }) => label === 'Confirmed' )?.value;
+
+  return `
+    <span class="icon-marker">
+      <span class="icon-marker-tooltip">
+        <h2>${header}</h2>
+        <ul>${statsString}</ul>
+      </span>
+      ${casesString}
+    </span>
+  `;
+}
