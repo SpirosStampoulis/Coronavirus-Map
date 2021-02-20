099cb9c41352b5cc7dc13480d2c5c71beaece1a3
Spiros Stampoulis <sstaboulis@gmail.com>
2021-02-20 14:59:05 +0100

    add components

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from 'components/Container';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <p>
+          <a href="http://spirosstampoulis.github.io/">Made by Spiros Stampoulis</a>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'components/Container';
+
+const Header = () => {
+  return (
+    <header>
+      <Container type="content">
+        <h1>Coronavirus (COVID-19)</h1>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
